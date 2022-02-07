e6dc37706a0fcece31c7df7f401941cd87692806
olivercito3 <58525006+olivercito3@users.noreply.github.com>
2022-02-07 17:00:07 +0100

    Add files via upload

---
+++ pancake/env.js
@@ -0,0 +1,11 @@
+const PRIVATE_KEY=''
+const TOKEN_ADDRESS='0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82';
+const AMOUNT=0.01
+const LEVEL=0.1
+
+module.exports = {
+    PRIVATE_KEY,
+    TOKEN_ADDRESS,
+    AMOUNT,
+    LEVEL
+};
